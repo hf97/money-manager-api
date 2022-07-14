2a97b983481623b8c2bb904a1b65282de1f9f4a5
HF97 <hugoanf97@gmail.com>
2022-07-14 20:22:13 +0100

    chore: add unit tests configuration

---
+++ MoneyManager.TESTS/Controllers/AccountsControllerTest.cs
@@ -0,0 +1,47 @@
+﻿using Autofac;
+using Autofac.Extras.Moq;
+using money_manager_api.Controllers;
+using money_manager_api.Entities;
+using money_manager_api.Helpers;
+using money_manager_api.Services;
+using MoneyManager.TESTS.Setup;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MoneyManager.TESTS.Controllers
+{
+    public class AccountsControllerTest
+    {
+        [Fact]
+        public async Task Get_Top_1()
+        {
+            using (var dataContextMock = DbContextMocker.GetEmptyDataContext())
+            {
+                var account = new Account
+                {
+
+                };
+
+                dataContextMock.Accounts.Add(Account);
+                dataContextMock.SaveChanges();
+
+                using (var mock = AutoMock.GetLoose(cfg => cfg.RegisterInstance(dataContextMock).As<DataContext>()))
+                {
+                    // arrange
+                    var accountService = mock.Create<AccountService>();
+
+                    var controller = new AccountsController(accountService);
+
+                    // act
+                    var actionResult = controller.GetAll().Take(1);
+
+                    // assert
+                    Assert.Single(actionResult.ToList());
+                }
+            }
+        }
+
+
+    }
+}
---
+++ MoneyManager.TESTS/Setup/DbContextMocker.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.EntityFrameworkCore;
+using money_manager_api.Entities;
+using money_manager_api.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyManager.TESTS.Setup
+{
+    public class DbContextMocker
+    {
+        public async Task<DataContext> GetFakeDataContextAsync()
+        {
+            var options = new DbContextOptionsBuilder<DataContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            var context = new DataContext(options);
+
+            await context.Accounts.AddAsync(
+                new Account
+                {
+
+                }
+            );
+
+            await context.SaveChangesAsync();
+
+            return context;
+        }
+
+        public static DataContext GetFakeDataContext()
+        {
+            var options = new DbContextOptionsBuilder<DataContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            var context = new DataContext(options);
+
+            context.Permissions.Add(
+                new Permission
+                {
+                    Id = 1,
+                    Name = "permission",
+                    Description = "permission description"
+                }
+            );
+
+            var currencyId = Guid.NewGuid();
+
+            context.Currencies.Add(
+                new Currency
+                {
+                    Id = currencyId,
+                    Iso = "EUR",
+                    Name = "Euro"
+                }
+            );
+
+            var colorId = Guid.NewGuid();
+
+            context.Colors.Add(
+                new Color
+                {
+                    Id = colorId,
+                    Name = "color",
+                    HexadecimalValue = "ffffff"
+                }
+            );
+
+            var userId = Guid.NewGuid();
+
+            context.Users.Add(
+                new User
+                {
+                    Id = userId,
+                    Email = "user@mail.com",
+                    FirstName = "firstName",
+                    LastName = "lastName",
+                    Username = "username",
+                    PermissionId = 1
+                }
+            );
+
+            var accountId = Guid.NewGuid();
+
+            context.Accounts.Add(
+                new Account
+                {
+                    Id = accountId,
+                    UserId = userId,
+                    Name = "account",
+                    CurrencyId = currencyId,
+                    ColorId = colorId,
+                    UseInStatistics = true,
+                    Archived = false
+                }
+            );
+
+            context.SaveChanges();
+
+            return context;
+        }
+
+        public static DataContext GetEmptyDataContext()
+        {
+            var options = new DbContextOptionsBuilder<DataContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            return new DataContext(options);
+        }
+    }
+}
